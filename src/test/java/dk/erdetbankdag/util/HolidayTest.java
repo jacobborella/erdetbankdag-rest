85256ccd9dc9e7c66fcb8d2f5d9456673fe66bef
Jacob Borella <jacobborella@hotmail.com>
2018-01-09 19:32:11 +0100

    changed junit implementation

--- src/test/java/dk/erdetbankdag/util/HolidayTest.java
+++ src/test/java/dk/erdetbankdag/util/HolidayTest.java
@@ -1,11 +1,10 @@
 package dk.erdetbankdag.util;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
-import org.junit.jupiter.api.Test;
 public class HolidayTest {
 	@Test
 	public void testEaster2018() {
@@ -13,7 +12,7 @@ public class HolidayTest {
 		cal.set(Calendar.MONTH, Calendar.APRIL);
 		cal.set(Calendar.DAY_OF_MONTH, 1);
 		cal.set(Calendar.YEAR, 2018);
-		assertTrue(Holiday.isItHoliday(cal), "April 1st is a Easter day");
+		assertTrue("April 1st is a Easter day", Holiday.isItHoliday(cal));
 	}
 	@Test
 	public void testIsBankday() {
@@ -21,15 +20,15 @@ public class HolidayTest {
 		cal.set(Calendar.MONTH, Calendar.JANUARY);
 		cal.set(Calendar.DAY_OF_MONTH, 8);
 		cal.set(Calendar.YEAR, 2018);
-		assertFalse(Holiday.isItHoliday(cal), "Monday jan 8th is a bankday");
+		assertFalse("Monday jan 8th is a bankday", Holiday.isItHoliday(cal));
 		cal.set(Calendar.DAY_OF_MONTH, 9);
-		assertFalse(Holiday.isItHoliday(cal), "Tuesday jan 9th is a bankday");
+		assertFalse("Tuesday jan 9th is a bankday", Holiday.isItHoliday(cal));
 		cal.set(Calendar.DAY_OF_MONTH, 10);
-		assertFalse(Holiday.isItHoliday(cal), "Wednesday jan 10th is a bankday");
+		assertFalse("Wednesday jan 10th is a bankday", Holiday.isItHoliday(cal));
 		cal.set(Calendar.DAY_OF_MONTH, 11);
-		assertFalse(Holiday.isItHoliday(cal), "Thursday jan 11th is a bankday");
+		assertFalse("Thursday jan 11th is a bankday", Holiday.isItHoliday(cal));
 		cal.set(Calendar.DAY_OF_MONTH, 12);
-		assertFalse(Holiday.isItHoliday(cal), "Friday jan 12th is a bankday");
+		assertFalse("Friday jan 12th is a bankday", Holiday.isItHoliday(cal));
 	}
 	@Test
 	public void testSunday() {
@@ -37,8 +36,8 @@ public class HolidayTest {
 		cal.set(Calendar.MONTH, Calendar.JANUARY);
 		cal.set(Calendar.YEAR, 2018);
 		cal.set(Calendar.DAY_OF_MONTH, 13);
-		assertTrue(Holiday.isItHoliday(cal), "Saturday jan 13th is a bankday");
+		assertTrue("Saturday jan 13th is a bankday", Holiday.isItHoliday(cal));
 		cal.set(Calendar.DAY_OF_MONTH, 14);
-		assertTrue(Holiday.isItHoliday(cal), "Sunday jan 14th isn't a bankday");
+		assertTrue("Sunday jan 14th isn't a bankday", Holiday.isItHoliday(cal));
 	}
 }
